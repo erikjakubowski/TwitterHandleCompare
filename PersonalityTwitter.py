844fb975991887f98cb025b8b3c632f80e49db3a
erikjakubowski <erik.jakubowski90@gmail.com>
2017-03-15 12:02:46 -0500

    Great way to check sources

---
+++ PersonalityTwitter.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  PersonalityTwitter.py
+#  
+#  Copyright 2017 Erik Jakubowski <erikjakubowski@Eriks-MacBook-Air.local>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+import operator
+import requests
+import json
+import twitter
+from watson_developer_cloud import PersonalityInsightsV2 as PersonalityInsights
+
+def analyze(handle):
+
+	twitter_consumer_key = ''
+	twitter_consumer_secret = ''
+	twitter_access_token = ''
+	twitter_access_secret = ''
+
+	twitter_api = twitter.Api(consumer_key=twitter_consumer_key,
+                  consumer_secret=twitter_consumer_secret,
+                  access_token_key=twitter_access_token,
+                  access_token_secret=twitter_access_secret)
+
+
+	statuses = twitter_api.GetUserTimeline(screen_name=handle, count=200, include_rts=False)
+
+	text = "" 
+	for s in statuses:
+	    if (s.lang =='en'):
+    			text += s.text.encode('utf-8')
+
+	pi_username = ''
+ 	pi_password = ''
+  
+  
+ 	personality_insights = 	PersonalityInsights(username=pi_username, password=pi_password)
+
+ 	pi_result = personality_insights.profile(text)
+
+	return pi_result
+
+def flatten(orig):
+    data = {}
+    for c in orig['tree']['children']:
+        if 'children' in c:
+            for c2 in c['children']:
+                if 'children' in c2:
+                    for c3 in c2['children']:
+                        if 'children' in c3:
+                            for c4 in c3['children']:
+                                if (c4['category'] == 'personality'):
+                                    data[c4['id']] = c4['percentage']
+                                    if 'children' not in c3:
+                                        if (c3['category'] == 'personality'):
+                                                data[c3['id']] = c3['percentage']
+    return data
+  
+def compare(dict1, dict2):
+	compared_data = {}
+	for keys in dict1:
+    		if dict1[keys] != dict2[keys]:
+			compared_data[keys] = abs(dict1[keys] - dict2[keys])
+	return compared_data
+
+user_handle = ""
+celebrity_handle = ""
+
+user_result = analyze(user_handle)
+celebrity_result = analyze(celebrity_handle)
+
+user = flatten(user_result)
+celebrity = flatten(celebrity_result)
+
+compared_results = compare(user,celebrity)
+
+sorted_result = sorted(compared_results.items(), 
+key=operator.itemgetter(1))
+
+for keys, value in sorted_result[:5]:
+    print keys,
+    print(user[keys]),
+    print ('->'),
+    print (celebrity[keys]),
+    print ('->'),
+    print (compared_results[keys])
+    print (sorted_result)
+		
+	
+    sys.exit(main(sys.argv))
